602f54859d293128e8f37fc9570c416da6977209
TristanKeith8420 <tristankeith8420@gmail.com>
2021-11-11 20:05:14 -0600

    refactor: Cleaned up code and added comments

--- src/EmotionIcon.py
+++ src/EmotionIcon.py
@@ -3,7 +3,7 @@ import discord  # The discord api
 # For type hinting
 from typing import List
 from typing import TextIO
-
+from typing import Union
 
 class EmotionIcon(discord.Client):
 
@@ -13,26 +13,41 @@ class EmotionIcon(discord.Client):
 
     # Called after it detects the messge is sent
     async def on_message(self, message: discord.Message) -> None:
+        # Don't do anything if the message is from the bot
         if(message.author.bot):
             return
-        # Split the message into strings
+
+        # Split the message into strings for parsing
         msg_split: List[str] = message.content.split()
 
-        emote_path: str = self.getEmoteFilepath(msg_split).strip()
-        if(emote_path == "Error"):
-            print("EmotionIcon: No emote sent")
-            return
+        # Gets the file path of the emote if it has one
+        emote_path: str = self.getEmoteFilepath(msg_split)
+        
+        if(emote_path is not None):
+            print("EmotionIcon: Preparing and sending emote")
+            emote_path = emote_path.strip()
+            await message.channel.send(file=discord.File(emote_path))
+            print("EmotionIcon: Emote sent") 
+        else:
+            print("EmotionIcon: No emote in message")
 
-        await message.channel.send(file=discord.File(emote_path))
 
-    def getEmoteFilepath(self, message: List[str]) -> str:
-        db: TextIO = open("../images/emotes.txt", "r")
-        for name_fp in db:
-            line: List[str] = name_fp.split(":")
+    # Is passed a list of strings that was the message and parses it for a valid emote
+    # by comparing it to our database, a text file lol, and seeing if its in there
+    # FIXME: Look into how inefficent this is to do
+    def getEmoteFilepath(self, message: List[str]) -> Union[str,None]:
+
+        db: TextIO = open("../images/emotes.txt", "r")  # Database file, just a text file
+        
+        for name_fp in db:  # For each line in the file <emotename>:<emotefilepath>
+            line: List[str] = name_fp.split(":").strip()
+            emotename: str = line[0]
+            emote_fp: str = line[1]
+            
+            # FIXME: This is the part that I think is inefficient
             for word in message:
-                if not (word == line[0]):
-                    continue
-                print("EmotionIcon: Found emote {0}".format(line[1].strip()))
-                return line[1]
-
-        return "Error"
+                if(word == emotename):
+                  print("EmotionIcon: Found emote {0}".format(emote_fp))
+                  return emote_fp
+        # No emote was found
+        return None
